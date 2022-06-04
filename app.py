7d4d7b277b95ffc6e71d33743e9424521e4b87ca
ChihYu Yeh <chihyu.jimmy.yeh@gmail.com>
2022-06-04 22:20:52 +0800

    refactor

--- app.py
+++ app.py
@@ -3,7 +3,10 @@ from py_code_analyzer.code_imports_analyzer import CodeImportsAnalyzer
 from py_code_analyzer.imports_graph_visualizer import ImportsGraphVisualizer
 
 python_files = CodeFetcher().get_python_files("cyyeh", "gradio", "gradio")
-code_imports_analyzer = (
-    CodeImportsAnalyzer(python_files).analyze_imports().generate_imports_graph()
+imports_graph = (
+    CodeImportsAnalyzer(python_files)
+    .analyze_imports()
+    .generate_imports_graph()
+    .imports_graph
 )
-ImportsGraphVisualizer().visualize(code_imports_analyzer.imports_graph)
+ImportsGraphVisualizer().visualize(imports_graph)
