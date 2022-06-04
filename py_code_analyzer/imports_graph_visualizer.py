5b4c973e8926d4f46a33fe947096280e0a7bb1a8
ChihYu Yeh <chihyu.jimmy.yeh@gmail.com>
2022-06-04 23:13:38 +0800

    display directed graph

--- py_code_analyzer/imports_graph_visualizer.py
+++ py_code_analyzer/imports_graph_visualizer.py
@@ -7,10 +7,11 @@ class ImportsGraphVisualizer:
     def visualize(
         cls,
         imports_graph: nx.Graph,
+        directed: bool = True,
         width: int = 100,
         height: int = 100,
         display_html_name: str = "nx.html",
     ):
-        _pyvis_network = Network(f"{width}%", f"{height}%")
+        _pyvis_network = Network(f"{width}%", f"{height}%", directed=directed)
         _pyvis_network.from_nx(imports_graph)
         _pyvis_network.show(display_html_name)
